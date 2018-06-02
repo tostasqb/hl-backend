b630a16f11e07dac79a8651fce3b75b5a60faeac
tostasqb <tostasqb@gmail.com>
2018-06-02 18:12:25 +0100

    Add item example controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_02_154849) do
+ActiveRecord::Schema.define(version: 2018_06_02_163553) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
