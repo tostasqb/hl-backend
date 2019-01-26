4e997e93400fb1c0fed3b6332dcbe3ca8c838d72
tostasqb <tostasqb@gmail.com>
2019-01-26 00:25:25 +0000

    Ignore old scripts in git

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ if User.first.nil?
   User.create!(
     name: 'Website',
     email: 'website@highline.com.pt',
-    password: ENV['AUTH_PASSWORD'] ,
+    password: ENV['AUTH_PASSWORD'],
     password_confirmation: ENV['AUTH_PASSWORD']
   )
 end
