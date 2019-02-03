66dbe2498cdcf1cc5372891d8762d45d168dee4e
tostasqb <tostasqb@gmail.com>
2019-02-03 23:26:40 +0000

    add product to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,5 @@ end
 if Product.first.nil?
   Product.create(menu_item_id: 1, title: 'Product One', description: 'First product description')
   Product.create(menu_item_id: 1, title: 'Product two', description: 'Another product')
+  Product.create(menu_item_id: 1, title: 'Mardi grás III', description: 'Cor Mardi grás collection disponível em 5 cores')
 end
