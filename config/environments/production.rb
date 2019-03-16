927eda35af3d853e9882762164818191b4d4e127
tostasqb <tostasqb@gmail.com>
2019-03-16 15:37:28 +0000

    Try to fix InvalidAuthentication with bundle update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.active_record.cache_versioning = false
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
