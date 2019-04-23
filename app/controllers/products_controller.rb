ac8083fde9d0d4ecbe6b1f1121abd0888f8a0793
tostasqb <tostasqb@gmail.com>
2019-04-23 20:15:09 +0100

    Show products based on filters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,21 +5,30 @@ class ProductsController < ApplicationController
   def index
     conditions = index_params
 
-    @products = Product.page(conditions[:page] || 1)
-                       .per(conditions[:per_page] || 3)
+    filter = JSON.parse(conditions[:filter])
+    filter.reject! { |a, v| v == false }
+
+    @products = if filter.blank?
+                  Product.page(conditions[:page] || 1)
+                    .per(conditions[:per_page] || 3)
+                else
+                  Product.joins(:tags)
+                    .where('tags.name' => filter.keys)
+                    .distinct
+                    .page(conditions[:page] || 1)
+                    .per(conditions[:per_page] || 3)
+                end
 
     render 'products/index'
-    # render json: @products
   end
 
   def show
     @product = Product.includes(:product_tags, :tags).find_by(id: params[:id])
 
     render 'products/show'
-    # render json: @product
   end
 
   def index_params
-    params.permit(:page, :per_page, :format)
+    params.permit(:page, :per_page, :filter, :format)
   end
 end
